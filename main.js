31e01e5f0eaed11c06a9900c4adcae5dbf28d20d
glgaviria64 <glgaviria@gmail.com>
2022-12-12 09:15:06 -0500

    Sprint finalizado

---
+++ main.js
@@ -0,0 +1,24 @@
+//  import data from './data.json' assert {type: 'json'};
+
+//  let Sec2Bars= document.querySelector('.Sec2_bars');
+ 
+// data.forEach(element => {
+// Sec2Bars.innerHTML += 
+// <div class="ChartBarras">
+// <div class="texto_top_barra">$17.45</div> 
+//   <div class="texto_bottom_barra">mon</div>
+// </div>
+// }); 
+
+// /* 
+//  let bars = document.querySelectorAll('.ChartBarras');
+// console.log(bars) */ 
+
+{/* <section class="Wrapper">
+    <article class="Wrapper1_Sec1">
+        <div class="Texts_Header">
+          <h4>My balance</h4>
+          <h2>$921.48</h2>
+        </div>
+          <img classsrc="img/logo.svg" alt="logo" />
+         </article> */}
